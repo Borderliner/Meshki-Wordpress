2282d839edf32a717f19292cd1834130f35f7f56
Borderliner <hajianpour.mr@gmail.com>
2016-09-24 15:08:25 +0330

    Add Meshki JS and CSS from cdnjs

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -9,10 +8,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title>Blog Template for Meshki</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Meshki CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/meshki/1.2.2/meshki.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="blog.css" rel="stylesheet">
@@ -162,10 +161,10 @@
     </footer>
 
 
-    <!-- Bootstrap core JavaScript
+    <!-- Meshki JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/meshki/1.2.2/meshki.min.js"></script>
   </body>
 </html>
